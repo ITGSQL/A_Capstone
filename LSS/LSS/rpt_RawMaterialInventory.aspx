e9182dabc19a8b071e1135db518a262dbe5a276e
Averitt <brian.averitt@gmail.com>
2017-01-18 16:22:56 -0500

    Report create for Raw Material Inventory

--- LSS/LSS/rpt_RawMaterialInventory.aspx
+++ LSS/LSS/rpt_RawMaterialInventory.aspx
@@ -1,5 +1,14 @@
 ﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Report.Master" CodeBehind="rpt_RawMaterialInventory.aspx.vb" Inherits="LSS.rpt_RawMaterialInventory" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+    <style>
+        .RED {
+            background-color: #ffc2c2;
+        }
+
+        .YELLOW {
+            background-color: #fff398;
+        }
+    </style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="BodyContent" runat="server">
     <h3>Raw Material Inventory</h3>
--- LSS/LSS/rpt_RawMaterialInventory.aspx.vb
+++ LSS/LSS/rpt_RawMaterialInventory.aspx.vb
@@ -6,6 +6,30 @@
 
             Dim strSQL As String = "Select * from [INVENTORY].[vw_Raw_Materials] order by category_name, name"
             Dim tblResults As DataTable = g_IO_Execute_SQL(strSQL, False)
+            If tblResults.Rows.Count > 0 Then
+                litReport.Text = "<table class=""table table-responsive table-bordered"">" &
+                    "<thead><tr><th>Name</th><th>Category</th><th>Brand</th><th>OnHand</th><th>Minimum</th></tr></thead>"
+                For Each row In tblResults.Rows
+                    Dim onhand As Decimal = row("ONHAND_QTY")
+                    Dim minimum As Decimal = row("MINIMUM_ONHAND_QTY")
+                    Dim strClass As String = ""
+
+                    If onhand < minimum Then
+                        strClass = "RED"
+                    ElseIf onhand = minimum Then
+                        strClass = "YELLOW"
+                    Else
+                        strClass = "GREEN"
+                    End If
+
+                    litReport.Text &= "<tr class=""" & strClass & """><td>" & row("Name") & "</td><td>" & row("Category_Name") & "</td><td>" & row("Brand") & "</td><td>" & row("ONHAND_QTY").ToString & "</td><td>" & row("MINIMUM_ONHAND_QTY") & "</td></tr>"
+                Next
+                litReport.Text &= "</table>"
+
+
+            Else
+                litReport.Text = "No Raw Materials found in the database at this time."
+            End If
 
         End If
     End Sub
