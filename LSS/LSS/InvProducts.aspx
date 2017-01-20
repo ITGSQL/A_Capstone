5829acf8242443dd099307007ac5a4053104cfe2
Averitt <brian.averitt@gmail.com>
2017-01-19 23:05:10 -0500

    Latest Changes from Home-Desktop

---
+++ LSS/LSS/InvProducts.aspx
@@ -0,0 +1,222 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="InvProducts.aspx.vb" Inherits="LSS.InvProducts" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="BodyContent" runat="server">
+     <span class="formTitle">Product Management</span>
+
+    <asp:Literal ID="litMessage" runat="server"></asp:Literal>
+    <asp:Panel ID="pnlRawMaterialListing" runat="server">
+        <table class="table table-responsive table-bordered">
+            <thead><tr><th>Name</th><th>Category</th><th>Brand</th><th>Description</th><th>Price</th><th>On-Hand</th><th><a href="InvRawMaterials.aspx?&action=NewRawMaterial"><img src="images/round_add_red.png" style="height: 16px; width: 16px;" /></a></th></tr></thead>
+            <asp:Repeater ID="rptrRawMaterialListing" runat="server">
+                <ItemTemplate>
+                    <tr><td><%#DataBinder.Eval(Container.DataItem, "Stock_Number") %></td>
+                        <td><%#DataBinder.Eval(Container.DataItem, "Category_Name") %></td>
+                        <td><%#DataBinder.Eval(Container.DataItem, "Brand") %></td>
+                        <td><%#DataBinder.Eval(Container.DataItem, "Description") %></td>
+                        <td><%#DataBinder.Eval(Container.DataItem, "PRICE") %></td>
+                        <td><%#DataBinder.Eval(Container.DataItem, "onhand_qty") %></td>
+                        <td><a href="InvProducts.aspx?id=<%#DataBinder.Eval(Container.DataItem, "Product_ID") %>&action=Update"><img src="images/edit.png" style="height: 16px; width: 16px;"/></a>
+                            &nbsp;&nbsp;&nbsp;&nbsp;<a href="InvProducts.aspx?id=<%#DataBinder.Eval(Container.DataItem, "Product_ID") %>&action=Delete"><img src="images/trash.png" style="height: 16px; width: 16px;" /></a>
+                            &nbsp;&nbsp;&nbsp;&nbsp;<a href="InvProducts.aspx?id=<%#DataBinder.Eval(Container.DataItem, "Product_ID") %>&action=Copy"><img src="images/copy.png" style="height: 16px; width: 16px;" /></a>
+                        </td></tr>
+                </ItemTemplate>
+            </asp:Repeater>
+
+        </table>
+
+    </asp:Panel>
+
+    <asp:Panel ID="pnlProductsUpdateDetails" runat="server">
+        <table class="table table-responsive table-bordered" style="max-width: 600px; float: left;">
+            <tr>
+                <td>Name: </td>
+                <td><asp:TextBox ID="txtName" runat="server" placeholder="Material Name..." Width="100%"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>
+                    Category: 
+                </td>
+                <td><asp:DropDownList ID="ddlCategory" runat="server"></asp:DropDownList><asp:ImageButton ID="imgNewCategory" runat="server" Height="16px" ImageUrl="~/images/round_add_red.PNG" Width="16px" /></td>
+            
+            </tr>
+            <tr>
+                <td>Brand: </td>
+                <td><asp:DropDownList ID="ddlBrand" runat="server"></asp:DropDownList><asp:ImageButton ID="imgNewBrand" runat="server" Height="16px" ImageUrl="~/images/round_add_red.PNG" Width="16px" /></td>       
+            </tr>
+            <tr>
+                <td>Description: </td>
+                <td><asp:TextBox ID="txtDescription" runat="server" placeholder="ex:Button,20mm,Plastic" Width="100%"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Unit of Measure:</td>
+                <td><asp:DropDownList ID="ddlUOM" runat="server"></asp:DropDownList><asp:ImageButton ID="imgNewUOM" runat="server" Height="16px" ImageUrl="~/images/round_add_red.PNG" Width="16px" /></td>       
+            </tr>
+            <tr>
+                <td>Price:</td>
+                <td><asp:TextBox ID="txtPrice" runat="server" placeholder="ex:299.99" Width="100%"></asp:TextBox></td>       
+            </tr>
+            <tr>
+                <td>On-Hand Qty:</td>
+                <td><asp:TextBox ID="txtOnHandQty" runat="server" placeholder="25" Width="100%"></asp:TextBox></td>       
+            </tr>
+            <tr>
+                <td>Minimum On-Hand Qty:</td>
+                <td><asp:TextBox ID="txtMinOnHandQty" runat="server" placeholder="5" Width="100%"></asp:TextBox></td>       
+            </tr>
+            <tr>
+                <td>Reorder Qty:</td>
+                <td><asp:TextBox ID="txtReorderQty" runat="server" placeholder="20" Width="100%"></asp:TextBox></td>       
+            </tr>
+            <tr><td colspan="2"><asp:Button ID="btnSaveNewRawMaterial" runat="server" Text="Save" />&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="btnCancelNewRawMaterial" runat="server" Text="Cancel" />&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="btnReturn" runat="server" Text="Return" /></td></tr>
+
+        </table>
+    </asp:Panel>
+
+    <asp:Panel ID="pnlRawMaterialProperties" runat="server">
+        <table class="table table-bordered table-responsive">
+            <thead><tr><th>Property</th><th>Value</th><th><asp:Literal ID="litNewPropertyImage" runat="server"></asp:Literal></th></tr></thead>
+            <asp:Repeater ID="rptrProperties" runat="server">
+                <ItemTemplate>
+                    
+                    <tr><td><%#DataBinder.Eval(Container.DataItem, "Property") %></td>
+                        <td><%#DataBinder.Eval(Container.DataItem, "VALUE") %></td>
+                        <td><a href="InvRawMaterials.aspx?id=<%#DataBinder.Eval(Container.DataItem, "PRODUCT_ID") %>&action=UpdateProperty&IPID=<%#DataBinder.Eval(Container.DataItem, "INVENTORY_PROPERTY_ID") %>"><img src="images/edit.png" /></a>
+                            &nbsp;&nbsp;&nbsp;&nbsp;<a href="InvRawMaterials.aspx?id=<%#DataBinder.Eval(Container.DataItem, "PRODUCT_ID") %>&action=DeleteProperty&IPID=<%#DataBinder.Eval(Container.DataItem, "INVENTORY_PROPERTY_ID") %>"><img src="images/trash.png" /></a>
+                        </td></tr>
+                </ItemTemplate>
+            </asp:Repeater>
+        </table>
+
+    </asp:Panel>
+
+    <asp:Panel ID="pnlAddMultiplePropertyToProduct" runat="server">
+        <table class="table table-responsive table-bordered" style="float: left; max-width: 300px;">
+            <tr><td colspan="2"><span style="font-size: 1.1em; font-weight: bold;">Add Properties</span></td></tr>
+            <tr>
+                <td>Property: </td>
+                <td><asp:DropDownList ID="ddlPropertiesAdd1" runat="server"></asp:DropDownList></td>
+                <td>Property Details: </td>
+                <td><asp:TextBox ID="txtAddPropertyDetails1" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Property: </td>
+                <td><asp:DropDownList ID="ddlPropertiesAdd2" runat="server"></asp:DropDownList></td>
+                <td>Property Details: </td>
+                <td><asp:TextBox ID="txtAddPropertyDetails2" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Property: </td>
+                <td><asp:DropDownList ID="ddlPropertiesAdd3" runat="server"></asp:DropDownList></td>
+                <td>Property Details: </td>
+                <td><asp:TextBox ID="txtAddPropertyDetails3" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Property: </td>
+                <td><asp:DropDownList ID="ddlPropertiesAdd4" runat="server"></asp:DropDownList></td>
+                <td>Property Details: </td>
+                <td><asp:TextBox ID="txtAddPropertyDetails4" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Property: </td>
+                <td><asp:DropDownList ID="ddlPropertiesAdd5" runat="server"></asp:DropDownList></td>
+                <td>Property Details: </td>
+                <td><asp:TextBox ID="txtAddPropertyDetails5" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Property: </td>
+                <td><asp:DropDownList ID="ddlPropertiesAdd6" runat="server"></asp:DropDownList></td>
+                <td>Property Details: </td>
+                <td><asp:TextBox ID="txtAddPropertyDetails6" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Property: </td>
+                <td><asp:DropDownList ID="ddlPropertiesAdd7" runat="server"></asp:DropDownList></td>
+                <td>Property Details: </td>
+                <td><asp:TextBox ID="txtAddPropertyDetails7" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Property: </td>
+                <td><asp:DropDownList ID="ddlPropertiesAdd8" runat="server"></asp:DropDownList></td>
+                <td>Property Details: </td>
+                <td><asp:TextBox ID="txtAddPropertyDetails8" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Property: </td>
+                <td><asp:DropDownList ID="ddlPropertiesAdd9" runat="server"></asp:DropDownList></td>
+                <td>Property Details: </td>
+                <td><asp:TextBox ID="txtAddPropertyDetails9" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Property: </td>
+                <td><asp:DropDownList ID="ddlPropertiesAdd10" runat="server"></asp:DropDownList></td>
+                <td>Property Details: </td>
+                <td><asp:TextBox ID="txtAddPropertyDetails10" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr><td colspan="2"><asp:Button ID="btnSaveAddMultipleProperty" runat="server" Text="Save" />&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="btnCancelAddMultipleProperty" runat="server" Text="Cancel" /></td></tr>
+        </table>
+    </asp:Panel>
+
+    <asp:Panel ID="pnlAddPropertyToProduct" runat="server">
+        <table class="table table-responsive table-bordered" style="float: left; max-width: 300px;">
+            <tr><td colspan="2"><span style="font-size: 1.1em; font-weight: bold;">Add Properties</span></td></tr>
+            <tr>
+                <td>Property: </td>
+                <td><asp:DropDownList ID="ddlPropertiesAdd" runat="server"></asp:DropDownList></td>
+            </tr>
+            <tr>
+                <td>Property Details: </td>
+                <td><asp:TextBox ID="txtAddPropertyDetails" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr><td colspan="2"><asp:Button ID="btnSaveAddProperty" runat="server" Text="Save" />&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="btnCancelAddProperty" runat="server" Text="Cancel" /></td></tr>
+        </table>
+    </asp:Panel>
+
+    <asp:Panel ID="pnlNewCategory" runat="server">
+        <table class="table table-responsive table-bordered">
+            <tr><td colspan="2"><span style="font-size: 1.1em; font-weight: bold;">New Category</span></td></tr>
+            <tr>
+                <td>Category Name: </td>
+                <td><asp:TextBox ID="txtNewCategory" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr><td colspan="2"><asp:Button ID="btnAddNewCategory" runat="server" Text="Save" />&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="btnCancelNewCategory" runat="server" Text="Cancel" /></td></tr>
+        </table>
+    </asp:Panel>
+
+    <asp:Panel ID="pnlNewBrand" runat="server">
+        <table class="table table-responsive table-bordered">
+            <tr><td colspan="2"><span style="font-size: 1.1em; font-weight: bold;">New Brand</span></td></tr>
+            <tr>
+                <td>Brand Name: </td>
+                <td><asp:TextBox ID="txtNewBrand" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr><td colspan="2"><asp:Button ID="btnSaveNewBrand" runat="server" Text="Save" />&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="btnCancelNewBrand" runat="server" Text="Cancel" /></td></tr>
+        </table>
+    </asp:Panel>
+
+    <asp:Panel ID="pnlNewUOM" runat="server">
+        <table class="table table-responsive table-bordered">
+            <tr><td colspan="2"><span style="font-size: 1.1em; font-weight: bold;">New Unit Of Measure</span></td></tr>
+            <tr>
+                <td>Unit Of Measure: </td>
+                <td><asp:TextBox ID="txtNewUOM" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr><td colspan="2"><asp:Button ID="btnSaveNewUOM" runat="server" Text="Save" />&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="btnCancelNewUOM" runat="server" Text="Cancel" /></td></tr>
+        </table>
+    </asp:Panel>
+
+    <asp:Panel ID="pnlNewProperty" runat="server">
+        <table class="table table-responsive table-bordered">
+            <tr><td colspan="2"><span style="font-size: 1.1em; font-weight: bold;">New Property</span></td></tr>
+            <tr>
+                <td>Property: </td>
+                <td><asp:TextBox ID="txtNewProperty" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr><td colspan="2"><asp:Button ID="btnSaveNewProperty" runat="server" Text="Save" />&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="btnCancelNewProperty" runat="server" Text="Cancel" /></td></tr>
+        </table>
+    </asp:Panel>
+
+
+
+
+</asp:Content>
---
+++ LSS/LSS/InvProducts.aspx.vb
@@ -0,0 +1,332 @@
+﻿Public Class InvProducts
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If IsPostBack Then
+            hidePanels()
+
+        Else
+
+        End If
+    End Sub
+
+    Private Sub disableForm()
+        txtName.Enabled = False
+        ddlCategory.Enabled = False
+        ddlBrand.Enabled = False
+        txtDescription.Enabled = False
+        ddlUOM.Enabled = False
+        txtOnHandQty.Enabled = False
+        txtPrice.Enabled = False
+        txtMinOnHandQty.Enabled = False
+        txtReorderQty.Enabled = False
+        btnSaveNewRawMaterial.Text = "Edit"
+        pnlRawMaterialProperties.Visible = True
+        btnCancelNewRawMaterial.Visible = False
+    End Sub
+
+    Private Sub enableForm()
+        txtName.Enabled = True
+        ddlCategory.Enabled = True
+        ddlBrand.Enabled = True
+        txtDescription.Enabled = True
+        ddlUOM.Enabled = True
+        txtOnHandQty.Enabled = True
+        txtPrice.Enabled = True
+        txtMinOnHandQty.Enabled = True
+        txtReorderQty.Enabled = True
+        btnSaveNewRawMaterial.Text = "Update"
+        pnlRawMaterialProperties.Visible = False
+        btnCancelNewRawMaterial.Visible = True
+    End Sub
+
+    Private Sub resetAllSubForms()
+        ''Properties Add
+        txtAddPropertyDetails.Text = ""
+        ddlPropertiesAdd.SelectedValue = -1
+        btnSaveAddProperty.Text = "Save"
+
+        ''New Category
+        txtNewCategory.Text = ""
+
+        ''New Brand
+        txtNewBrand.Text = ""
+
+        ''New YOM
+        txtNewUOM.Text = ""
+    End Sub
+
+    Private Sub hidePanels()
+
+        pnlAddPropertyToProduct.Visible = False
+        pnlAddMultiplePropertyToProduct.Visible = False
+
+        pnlNewBrand.Visible = False
+        pnlNewCategory.Visible = False
+        pnlNewProperty.Visible = False
+        pnlNewUOM.Visible = False
+
+        pnlRawMaterialListing.Visible = False
+        pnlRawMaterialProperties.Visible = False
+        pnlProductsUpdateDetails.Visible = False
+
+    End Sub
+
+    Private Sub loadDropDowns()
+        loadBrands()
+        loadUOM()
+        loadCategories()
+        loadProperties()
+    End Sub
+
+    Private Sub loadRawMaterialProperties()
+        Dim strSQL As String = "Select * from [INVENTORY].[VW_INVENTORY_PROPERTY] WHERE PRODUCT_ID = " & Request.QueryString("id") & " And IS_PRODUCT = 0 ORDER BY NAME"
+        Dim tblResults As DataTable = g_IO_Execute_SQL(strSQL, False)
+        If tblResults.Rows.Count > 0 Then
+            rptrProperties.DataSource = tblResults
+            rptrProperties.DataBind()
+        End If
+        litNewPropertyImage.Text = "<a href=""InvRawMaterials.aspx?id=" & Request.QueryString("ID") & "&action=AddProperty""><img src=""images/round_add_red.png"" style=""height: 16px; width: 16px;"" /></a>"
+    End Sub
+
+    Private Sub loadBrands()
+        Dim strSQL As String = "SELECT brand_id, [brand] FROM [inventory].brand ORDER BY [brand]"
+        Dim tblResults As DataTable = g_IO_Execute_SQL(strSQL, False)
+        createDefaultRow(tblResults, "--Brand--", "Brand", "Brand_id")
+        ddlBrand.DataValueField = "brand_id"
+        ddlBrand.DataTextField = "Brand"
+        ddlBrand.DataSource = tblResults
+        ddlBrand.DataBind()
+    End Sub
+
+    Private Sub loadProperties()
+        Dim strSQL As String = "SELECT PROPERTY_ID, [PROPERTY] FROM [inventory].[PROPERTY] ORDER BY [PROPERTY]"
+        Dim tblResults As DataTable = g_IO_Execute_SQL(strSQL, False)
+        createDefaultRow(tblResults, "--Property--", "Property", "Property_id")
+        ddlPropertiesAdd.DataValueField = "Property_id"
+        ddlPropertiesAdd.DataTextField = "Property"
+        ddlPropertiesAdd.DataSource = tblResults
+        ddlPropertiesAdd.DataBind()
+    End Sub
+
+    Private Sub loadMultipleProperties()
+        Dim strSQL As String = "SELECT PROPERTY_ID, [PROPERTY] FROM [inventory].[PROPERTY] ORDER BY [PROPERTY]"
+        Dim tblResults As DataTable = g_IO_Execute_SQL(strSQL, False)
+        createDefaultRow(tblResults, "--Property--", "Property", "Property_id")
+
+        ddlPropertiesAdd1.DataValueField = "Property_id"
+        ddlPropertiesAdd1.DataTextField = "Property"
+        ddlPropertiesAdd1.DataSource = tblResults
+        ddlPropertiesAdd1.DataBind()
+
+        ddlPropertiesAdd2.DataValueField = "Property_id"
+        ddlPropertiesAdd2.DataTextField = "Property"
+        ddlPropertiesAdd2.DataSource = tblResults
+        ddlPropertiesAdd2.DataBind()
+
+        ddlPropertiesAdd3.DataValueField = "Property_id"
+        ddlPropertiesAdd3.DataTextField = "Property"
+        ddlPropertiesAdd3.DataSource = tblResults
+        ddlPropertiesAdd3.DataBind()
+
+        ddlPropertiesAdd4.DataValueField = "Property_id"
+        ddlPropertiesAdd4.DataTextField = "Property"
+        ddlPropertiesAdd4.DataSource = tblResults
+        ddlPropertiesAdd4.DataBind()
+
+        ddlPropertiesAdd5.DataValueField = "Property_id"
+        ddlPropertiesAdd5.DataTextField = "Property"
+        ddlPropertiesAdd5.DataSource = tblResults
+        ddlPropertiesAdd5.DataBind()
+
+        ddlPropertiesAdd6.DataValueField = "Property_id"
+        ddlPropertiesAdd6.DataTextField = "Property"
+        ddlPropertiesAdd6.DataSource = tblResults
+        ddlPropertiesAdd6.DataBind()
+
+        ddlPropertiesAdd7.DataValueField = "Property_id"
+        ddlPropertiesAdd7.DataTextField = "Property"
+        ddlPropertiesAdd7.DataSource = tblResults
+        ddlPropertiesAdd7.DataBind()
+
+        ddlPropertiesAdd8.DataValueField = "Property_id"
+        ddlPropertiesAdd8.DataTextField = "Property"
+        ddlPropertiesAdd8.DataSource = tblResults
+        ddlPropertiesAdd8.DataBind()
+
+        ddlPropertiesAdd9.DataValueField = "Property_id"
+        ddlPropertiesAdd9.DataTextField = "Property"
+        ddlPropertiesAdd9.DataSource = tblResults
+        ddlPropertiesAdd9.DataBind()
+
+        ddlPropertiesAdd10.DataValueField = "Property_id"
+        ddlPropertiesAdd10.DataTextField = "Property"
+        ddlPropertiesAdd10.DataSource = tblResults
+        ddlPropertiesAdd10.DataBind()
+
+    End Sub
+
+    Private Sub loadUOM()
+        Dim strSQL As String = "SELECT UOM_ID, [UOM] FROM [inventory].UOM ORDER BY [UOM]"
+        Dim tblResults As DataTable = g_IO_Execute_SQL(strSQL, False)
+        createDefaultRow(tblResults, "--UOM--", "UOM", "UOM_id")
+        ddlUOM.DataValueField = "UOM_id"
+        ddlUOM.DataTextField = "UOM"
+        ddlUOM.DataSource = tblResults
+        ddlUOM.DataBind()
+    End Sub
+
+    Private Sub loadCategories()
+        Dim strSQL As String = "SELECT CATEGORY_ID, [CATEGORY_NAME] FROM [inventory].CATEGORY ORDER BY [CATEGORY_NAME]"
+        Dim tblResults As DataTable = g_IO_Execute_SQL(strSQL, False)
+        createDefaultRow(tblResults, "--CATEGORY--", "CATEGORY_NAME", "CATEGORY_ID")
+        ddlCategory.DataValueField = "CATEGORY_ID"
+        ddlCategory.DataTextField = "CATEGORY_NAME"
+        ddlCategory.DataSource = tblResults
+        ddlCategory.DataBind()
+    End Sub
+
+    Protected Sub btnCancelNewRawMaterial_Click(sender As Object, e As EventArgs) Handles btnCancelNewRawMaterial.Click
+        Response.Redirect("InvProducts.aspx?action=update&id=" & Request.QueryString("ID"))
+    End Sub
+
+    Protected Sub imgNewCategory_Click(sender As Object, e As ImageClickEventArgs) Handles imgNewCategory.Click
+        hidePanels()
+        pnlNewCategory.Visible = True
+    End Sub
+
+    Protected Sub imgNewBrand_Click(sender As Object, e As ImageClickEventArgs) Handles imgNewBrand.Click
+        hidePanels()
+        pnlNewBrand.Visible = True
+    End Sub
+
+    Protected Sub imgNewUOM_Click(sender As Object, e As ImageClickEventArgs) Handles imgNewUOM.Click
+        hidePanels()
+        pnlNewUOM.Visible = True
+    End Sub
+
+    Protected Sub btnCancelNewCategory_Click(sender As Object, e As EventArgs) Handles btnCancelNewCategory.Click
+        hidePanels()
+        txtNewCategory.Text = ""
+        pnlProductsUpdateDetails.Visible = True
+    End Sub
+
+    Protected Sub btnCancelNewBrand_Click(sender As Object, e As EventArgs) Handles btnCancelNewBrand.Click
+        hidePanels()
+        txtNewBrand.Text = ""
+        pnlProductsUpdateDetails.Visible = True
+    End Sub
+
+    Protected Sub btnCancelNewUOM_Click(sender As Object, e As EventArgs) Handles btnCancelNewUOM.Click
+        hidePanels()
+        txtNewUOM.Text = ""
+        pnlProductsUpdateDetails.Visible = True
+    End Sub
+
+    Protected Sub btnCancelNewProperty_Click(sender As Object, e As EventArgs) Handles btnCancelNewProperty.Click
+        hidePanels()
+        txtNewProperty.Text = ""
+        pnlProductsUpdateDetails.Visible = True
+    End Sub
+
+    Protected Sub btnSaveNewProperty_Click(sender As Object, e As EventArgs) Handles btnSaveNewProperty.Click
+        If txtNewProperty.Text <> "" Then
+            Try
+                Dim StrSQL As String = "Insert into [INVENTORY].[PROPERTY] (PROPERTY) VALUES ('" & txtNewProperty.Text.Replace("'", "''") & "')"
+                Dim returnInt As Integer = g_IO_Execute_SQL(StrSQL, False).Rows(0)(0)
+                loadProperties()
+                ddlPropertiesAdd.SelectedValue = returnInt
+                hidePanels()
+                resetAllSubForms()
+                pnlProductsUpdateDetails.Visible = True
+            Catch ex As Exception
+                txtNewProperty.BorderColor = System.Drawing.Color.Red
+                litMessage.Text = "<span class=""fontRed"">Invalid Property Name Provided.</span><br /><br />"
+            End Try
+        End If
+
+    End Sub
+
+    Protected Sub btnAddNewCategory_Click(sender As Object, e As EventArgs) Handles btnAddNewCategory.Click
+        If txtNewCategory.Text <> "" Then
+            Try
+                Dim StrSQL As String = "Insert into [INVENTORY].[CATEGORY] (CATEGORY_NAME) VALUES ('" & txtNewCategory.Text.Replace("'", "''") & "')"
+                Dim returnInt As Integer = g_IO_Execute_SQL(StrSQL, False).Rows(0)(0)
+                loadCategories()
+                ddlCategory.SelectedValue = returnInt
+                hidePanels()
+                resetAllSubForms()
+                pnlProductsUpdateDetails.Visible = True
+            Catch ex As Exception
+                txtNewCategory.BorderColor = System.Drawing.Color.Red
+                litMessage.Text = "<span class=""fontRed"">Invalid Category Name Provided.</span><br /><br />"
+            End Try
+        End If
+    End Sub
+
+    Protected Sub btnSaveNewBrand_Click(sender As Object, e As EventArgs) Handles btnSaveNewBrand.Click
+        If txtNewBrand.Text <> "" Then
+            Try
+                Dim StrSQL As String = "Insert into [INVENTORY].[BRAND] (BRAND) VALUES ('" & txtNewBrand.Text.Replace("'", "''") & "')"
+                Dim returnInt As Integer = g_IO_Execute_SQL(StrSQL, False).Rows(0)(0)
+                loadBrands()
+                ddlBrand.SelectedValue = returnInt
+                hidePanels()
+                resetAllSubForms()
+                pnlProductsUpdateDetails.Visible = True
+            Catch ex As Exception
+                txtNewBrand.BorderColor = System.Drawing.Color.Red
+                litMessage.Text = "<span class=""fontRed"">Invalid Brand Name Provided.</span><br /><br />"
+            End Try
+        End If
+    End Sub
+
+    Protected Sub btnSaveNewUOM_Click(sender As Object, e As EventArgs) Handles btnSaveNewUOM.Click
+        If txtNewUOM.Text <> "" Then
+            Try
+                Dim StrSQL As String = "Insert into [INVENTORY].[UOM] (UOM) VALUES ('" & txtNewUOM.Text.Replace("'", "''") & "')"
+                Dim returnInt As Integer = g_IO_Execute_SQL(StrSQL, False).Rows(0)(0)
+                loadUOM()
+                ddlUOM.SelectedValue = returnInt
+                hidePanels()
+                resetAllSubForms()
+                pnlProductsUpdateDetails.Visible = True
+            Catch ex As Exception
+                txtNewUOM.BorderColor = System.Drawing.Color.Red
+                litMessage.Text = "<span class=""fontRed"">Invalid Unit Of Measure Provided.</span><br /><br />"
+            End Try
+        End If
+    End Sub
+
+    Protected Sub btnSaveAddProperty_Click(sender As Object, e As EventArgs) Handles btnSaveAddProperty.Click
+        If ddlPropertiesAdd.SelectedValue <> -1 And txtAddPropertyDetails.Text <> "" Then
+            If btnSaveNewProperty.Text = "Update" Then
+                Try
+                    Dim strsQL As String = "UPDATE [INVENTORY].[INVENTORY_PROPERTY] SET " &
+                        "PRODUCT_ID = " & Request.QueryString("ID") & ", " &
+                        "PROPERTY_ID = " & ddlPropertiesAdd.SelectedValue & ", " &
+                        "VALUE = '" & txtAddPropertyDetails.Text.Replace("'", "''") & "' " &
+                        "WHERE INVENTORY_PROPERTY_ID = " & Request.QueryString("IPID")
+                    g_IO_Execute_SQL(strsQL, False)
+                Catch ex As Exception
+                    litMessage.Text = "<span class=""fontRed"">An error has occurred.</span><br /><br />"
+                End Try
+            Else
+                Try
+                    Dim strsQL As String = "INSERT into [INVENTORY].[INVENTORY_PROPERTY] (PRODUCT_ID, PROPERTY_ID, VALUE, IS_PRODUCT) VALUES " &
+                    "(" & Request.QueryString("ID") & "," & ddlPropertiesAdd.SelectedValue & ",'" & txtAddPropertyDetails.Text.Replace("'", "''") & "',1)"
+                    g_IO_Execute_SQL(strsQL, False)
+                Catch ex As Exception
+                    litMessage.Text = "<span class=""fontRed"">An error has occurred.</span><br /><br />"
+                End Try
+            End If
+            Response.Redirect("InvRawMaterials.aspx?action=update&id=" & Request.QueryString("ID"))
+        End If
+    End Sub
+
+    Private Sub addPropertyToProduct(ByVal property_id As Integer, ByVal value As String)
+        Dim strsQL As String = "INSERT into [INVENTORY].[INVENTORY_PROPERTY] (PRODUCT_ID, PROPERTY_ID, VALUE, IS_PRODUCT) VALUES " &
+                    "(" & Request.QueryString("ID") & "," & property_id & ",'" & value.Replace("'", "''") & "',1)"
+        g_IO_Execute_SQL(strsQL, False)
+    End Sub
+End Class
